131618bbe0687335878c82bf0ab46142f9d4badb
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2023-02-21 03:14:53 +0600

    GPT3_JSM: Navbar design completed

--- gpt3_jsm/src/App.js
+++ gpt3_jsm/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Cta, Brand, Navbar } from './components'
-import { Footer, Blog, Possibility, Feature, WhatGPT3, Header} from './containers';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+         <Navbar/>
+         <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
