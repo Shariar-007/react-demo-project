804453ed7daba488692457bf73656fed730b6820
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2023-06-11 17:47:20 +0600

    gericht-restaurant: gallery component design done

--- gericht-restaurant/src/containers/Intro/Intro.jsx
+++ gericht-restaurant/src/containers/Intro/Intro.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './Intro.css';
+import React, { useState } from "react";
+import "./Intro.css";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+              setPlayVideo(!playVideo);
+              if (playVideo) {
+                vidRef.current.pause();
+              } else {
+                vidRef.current.play();
+              }
+          }}
+        >
+          { playVideo ? (
+            <BsPauseFill color="#fff" frontSize={30} />
+            ) : (
+            <BsFillPlayFill color="#fff" frontSize={30}/>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
