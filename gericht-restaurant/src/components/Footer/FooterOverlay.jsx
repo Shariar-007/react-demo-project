4d502bfe68d8b01024b97bf007238f61c1893f82
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2023-06-12 15:33:43 +0600

    gericht-restaurant: footer design done

--- gericht-restaurant/src/components/Footer/FooterOverlay.jsx
+++ gericht-restaurant/src/components/Footer/FooterOverlay.jsx
@@ -3,8 +3,10 @@ import './FooterOverlay.css';
 
 const FooterOverlay = () => {
   return (
-    <div>
-      FooterOverlay
+    <div className="app__footerOverlay">
+      <div className='app__footerOverlay-black'>
+      <div className="app__footerOverlay-img app__bg" />
+      </div>
     </div>
   )
 }
