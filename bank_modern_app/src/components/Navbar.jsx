9df9603ab99e8be2d21bac193193d8db90ac4cae
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2023-06-15 12:37:01 +0600

    bank_modern_app:  hero section completed

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -12,8 +12,7 @@ const Navbar = () => {
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
+          <li key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] 
             ${active === nav.title ? "text-white" : "text-dimWhite"} ${
               index === navLinks.length - 1 ? "mr-0" : "mr-10"
@@ -34,8 +33,7 @@ const Navbar = () => {
         />
 
         <div
-          className={`${
-            !toggle ? "hidden" : "flex"
+          className={`${ !toggle ? "hidden" : "flex"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
