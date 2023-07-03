7415f6bbdeec195fbde9458edcea9b508bc6dfb8
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2023-07-03 19:03:45 +0600

    project gtm-exercise: searchable component design and api integration completed

--- gym_exercises/src/pages/Home.js
+++ gym_exercises/src/pages/Home.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Box } from '@mui/material';
 import HeroBanner from './../components/HeroBanner';
 import SearchExercises from './../components/SearchExercises';
 import Exercises from './../components/Exercises';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
+
   return (
     <Box>
       <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+      <SearchExercises bodyPart={bodyPart} setBodyPart={setBodyPart} setExercises={setExercises}/>
+      <Exercises bodyPart={bodyPart} setBodyPart={setBodyPart} setExercises={setExercises}/>
     </Box>
   )
 }
